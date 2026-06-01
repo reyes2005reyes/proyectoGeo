4e900d26b245c69dca888b165f1ca8ca4825fb1e
Jhoan Guevara <jhoan07@soy.sena.edu.co>
2026-05-31 20:58:09 -0500

    Corregida ruta en listaUsuarios

--- controller/listaUsuarios/listaUsuariosController.php
+++ controller/listaUsuarios/listaUsuariosController.php
@@ -3,7 +3,7 @@
 class listaUsuariosController {
 
     public function lista() {
-        require_once __DIR__ . '/../../view/listaUsuarios/listaUsuarios.php';
+        require_once __DIR__ . '/../../view/listaUsuarios/listaUsuarios.php'; // Se corrigio la ruta del archivo de vista para que apunte a la carpeta correcta
     }
 }
 ?>
